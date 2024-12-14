c360e8c7cb732753ef997cf631575fbf626990ca
Andrii Rudenko <rssuand@gmail.com>
2024-12-14 21:37:57 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function calcAverageCalories(days) {
+  let sumCalories = 0;
+  let sumDays = 0;
+  for (const element of days) {
+    sumCalories += element.calories;
+    ++sumDays;
+  }
+  return sumDays ? sumCalories / sumDays : 0;
+}
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
+
